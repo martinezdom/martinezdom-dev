44ff02c8c1328ea7966a3068e1f7228c515f6f2f
Miguel Ángel Martínez <tartanasdev@gmail.com>
2025-03-24 22:39:07 +0100

    Updates

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,24 @@
 </script>
 
 <template>
+  <header>
+    <a href="#"><img src="#" alt="Logo"></a>
+    <nav>
+      <ul>
+        <li>Inicio</li>
+        <li>Experiencia</li>
+        <li>Estudios</li>
+        <li>Contacto</li>
+      </ul>
+    </nav>
+  </header>
   <main>
-    <h1>martinezdom</h1>
+    <h1>Miguel Ángel Martínez Domínguez</h1>
+    <h2>Desarrollador Fullstack Junior</h2>
+    <p>¡Hola!
+Soy Miguel Ángel, un apasionado de la programación y la tecnología.
+Me encanta aprender cosas nuevas y superar retos.
+Soy un desarrollador Fullstack Junior con experiencia en tecnologías como HTML, CSS, JavaScript, Vue.js, MySQL, Git, GitHub, entre otras.
+    </p>
   </main>
 </template>
