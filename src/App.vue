3836729449226529fd4fdc7b901c6fa652ae5004
Miguel Ángel Martínez <tartanasdev@gmail.com>
2025-03-24 10:50:51 +0100

    First commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import { mapActions } from 'pinia';
+import { useDataStore } from './stores/store';
+export default {
+  components: {
+  },
+  methods: {
+    ...mapActions(useDataStore, ['']),
+  },
+  mounted() {
+  }
+}
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+
+<style scoped>
+@import url('https://fonts.googleapis.com/icon?family=Material+Icons');
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<script>
+</script>
+
+<template>
+  <main>
+    <h1>martinezdom</h1>
+  </main>
+</template>
